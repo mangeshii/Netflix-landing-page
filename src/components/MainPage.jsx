3c9578277555d1f9403f68d9c4e312d7650806ab
Mangeshi <thisismangeshi@gmail.com>
2021-11-05 16:44:36 +0530

    added second page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,22 +1,38 @@
-import "./MainPage.css";
+import "./MainPage.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../components/Second-page"
+import SecondPage from "../components/Second-page";
 
 const MainPage = () => {
     return (
-        <div className="container-fluid main-page d-flex justify-content-center align-items-center">
-            <div className="title">
-                <h1>Unlimited movies, TV shows and more.</h1>
-                <h3>Watch anywhere. Cancel anytime.</h3>
-                <h5>Ready to watch? Enter your email to create or restart your membership.</h5>
+        <>
+            <div className="container-fluid main-page d-flex justify-content-center align-items-center">
+                <div className="cont">
 
-                <form className="form">
-                    <div className="email">
-                        <input type="email" name="email" placeholder="Email address"/>
-                        <button className="bg-danger">Get Started</button>
-                    </div>
-                </form>
+                <div className="title">
+                    <h1>Unlimited movies, TV shows and more.</h1>
+                    <h3>Watch anywhere. Cancel anytime.</h3>
+                    <h5>
+                        Ready to watch? Enter your email to create or restart
+                        your membership.
+                    </h5>
+
+                    <form className="form col-lg-10">
+                        <div className="email">
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email address"
+                            />
+                            <button className="bg-danger">Get Started</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+
             </div>
-        </div>
+            <SecondPage/>
+        </>
     );
 };
 export default MainPage;
---
+++ src/components/Second-page.jsx
@@ -0,0 +1,29 @@
+import "./SecondPage.scss";
+import netflix_tv from "../Images/netflix-tv.png";
+const SecondPage = () => {
+    return (
+        <div className="second-page container-fluid d-flex justify-content-center align-items-center">
+            <div className="row">
+                <div className="col-8 mx-auto">
+                    <div className="row">
+                        <div className="col-md-6 pt-5 pt-lg-0 order-2 order-sm-1 d-flex justify-content-center flex-column">
+                            <h1>Enjoy on your TV.</h1>
+                            <h4>
+                                Watch on smart TVs, PlayStation, Xbox,
+                                Chromecast, Apple TV, Blu-ray players and more.
+                            </h4>
+                        </div>
+                        <div className="col-md-6 pt-5 pt-lg-0 order-1 order-sm-2">
+                            <img
+                                className="img-fluid"
+                                src={netflix_tv}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default SecondPage;
